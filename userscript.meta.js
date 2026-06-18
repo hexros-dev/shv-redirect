37995bd7119f611099bbc4d7f32403032f1d33ea
hexros_1337 <ngotanvn2001@gmail.com>
2026-06-18 14:02:11 +0700

    [ver 1.5] add icon

--- userscript.meta.js
+++ userscript.meta.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name            Auto Redirect SacHiepVien Link
 // @namespace       https://hexros.qzz.io
-// @version         1.4
+// @version         1.5
 // @match           https://sachiepvien.net/redirect.html*
 // @run-at          document-start
 // @grant           GM_xmlhttpRequest
 // @connect         link.sachiepvien.net
 // @connect         mega.nz
 // @author          Hexros Raymond
+// @icon            https://raw.githubusercontent.com/hexros-dev/shv-redirect/refs/heads/master/assets/logo.png
 // @updateURL       https://raw.githubusercontent.com/hexros-dev/shv-redirect/refs/heads/master/userscript.meta.js
 // @downloadURL     https://raw.githubusercontent.com/hexros-dev/shv-redirect/refs/heads/master/userscript.user.js
 // ==/UserScript==
