e2e28d3f1fffdae48d277221178dea6adae90bd7
Hexros <hexros.dev@gmail.com>
2026-06-18 10:51:16 +0700

    Create userscript.meta.js

---
+++ userscript.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name            Auto Redirect SacHiepVien Link
+// @namespace       https://hexros.qzz.io
+// @version         1.4
+// @match           https://sachiepvien.net/redirect.html*
+// @run-at          document-start
+// @grant           GM_xmlhttpRequest
+// @connect         link.sachiepvien.net
+// @connect         mega.nz
+// @author          Hexros Raymond
+// @updateURL       https://raw.githubusercontent.com/hexros-dev/shv-redirect/refs/heads/master/userscript.meta.js
+// @downloadURL     https://raw.githubusercontent.com/hexros-dev/shv-redirect/refs/heads/master/userscript.user.js
+// ==/UserScript==
