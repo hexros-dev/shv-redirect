6009c295879eb221ce411735e4b575d56adcd5fb
Hexros <hexros.dev@gmail.com>
2026-06-18 10:34:16 +0700

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,49 +1,72 @@
 // ==UserScript==
 // @name            Auto Redirect SacHiepVien Link
 // @namespace       https://hexros.qzz.io
-// @version         1.1
+// @version         1.2
 // @match           https://sachiepvien.net/redirect.html*
 // @run-at          document-start
-// @grant           none
+// @grant           GM_xmlhttpRequest
+// @connect         link.sachiepvien.net
+// @connect         mega.nz
 // @author          Hexros Raymond
-// @downloadURL     https://raw.githubusercontent.com/hexros-dev/shv-redirect/refs/heads/master/userscript.js?token=GHSAT0AAAAAAD6RQUQGNDWNAA2T6IKSBWCC2RPY7HA
 // ==/UserScript==
 
 (function () {
     "use strict";
 
+    function resolveAndRedirect(url) {
+        GM_xmlhttpRequest({
+            method: "GET",
+            url,
+            timeout: 10000,
+            onload(response) {
+                const finalUrl = response.finalUrl;
+                if (finalUrl && finalUrl !== url) {
+                    location.replace(finalUrl);
+                    return;
+                }
+                location.replace(url);
+            },
+            onerror() {
+                location.replace(url);
+            },
+            ontimeout() {
+                location.replace(url);
+            }
+        });
+    }
+    
     function tryRedirect() {
         const link = document.getElementById("redirectLink");
-        if (!link) return false;
-
-        const href = link.href;
-        if (!href) return false;
-
+        if (!link?.href) {
+            return false;
+        }
         try {
-            const url = new URL(href);
+            const url = new URL(link.href);
             let targetUrl = url.searchParams.get("url");
-
-            if (!targetUrl) return false;
-
-            targetUrl = decodeURIComponent(targetUrl);
-
-            if (/^https:\/\/link\.sachiepvien\.net\//i.test(targetUrl)) {
-                location.replace(targetUrl);
-                return true;
+            if (!targetUrl) {
+                return false;
             }
-        } catch (e) {}
-
-        return false;
+            targetUrl = decodeURIComponent(targetUrl);
+            if (!/^https:\/\/link\.sachiepvien\.net\//i.test(targetUrl)) {
+                return false;
+            }
+            resolveAndRedirect(targetUrl);
+            return true;
+        } catch {
+            return false;
+        }
     }
-
-    if (tryRedirect()) return;
-
+    
+    if (tryRedirect()) {
+        return;
+    }
+    
     const observer = new MutationObserver(() => {
         if (tryRedirect()) {
             observer.disconnect();
         }
     });
-
+    
     observer.observe(document.documentElement, {
         childList: true,
         subtree: true
