8c53d5306f3458ddf417c00798c3f9dd2b9fdd4c
Hexros <hexros.dev@gmail.com>
2026-06-18 10:50:48 +0700

    Update and rename userscript.js to userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name            Auto Redirect SacHiepVien Link
 // @namespace       https://hexros.qzz.io
-// @version         1.3
+// @version         1.4
 // @match           https://sachiepvien.net/redirect.html*
 // @run-at          document-start
 // @grant           GM_xmlhttpRequest
 // @connect         link.sachiepvien.net
 // @connect         mega.nz
 // @author          Hexros Raymond
-// @downloadUrl     https://raw.githubusercontent.com/hexros-dev/shv-redirect/refs/heads/master/userscript.js
+// @updateURL       https://raw.githubusercontent.com/hexros-dev/shv-redirect/refs/heads/master/userscript.meta.js
+// @downloadURL     https://raw.githubusercontent.com/hexros-dev/shv-redirect/refs/heads/master/userscript.user.js
 // ==/UserScript==
 
 (function () {
